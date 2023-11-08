d4a860612864a5043a4d63e9d6b360dcded638f6
Dario <marcosdemo232527@gmail.com>
2023-11-08 19:13:43 -0300

    Planteo de dudas y tareas pendientes

--- escuela7708/src/main/java/com/example/escuela7708/dario.java
+++ escuela7708/src/main/java/com/example/escuela7708/dario.java
@@ -1,4 +1,9 @@
 package com.example.escuela7708;
 
 public class dario {
+    //hola soy un comentario
+    //DOCENTES/ALUMNOS: Al agregar estudiantes el calendario no te tiene que dejar seleccionar CUALQUIER fecha de nacimiento.
+    //CALENDARIO: Seleccionas docentes en X fecha, podes seleccionar muchas veces en un solo día al mismo, no te deja sacarlo.
+    //CALENDARIO: Deberia poder contemplar los dias que hay jornada o feriado no van profesores.
+    //CALENDARIO: Los menu button se crean desde el controlador, no se como puedo acceder a eso
 }
